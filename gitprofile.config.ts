d4b294ad755ab2600ccf77ce0106785002c036ad
Dillon Luong <60891309+dillon-luong@users.noreply.github.com>
2026-09-13 18:38:32 -0400

    trying to add github projects manually
    Changed GitHub projects mode from 'automatic' to 'manual' and updated the list of manually specified projects.

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -14,7 +14,7 @@ const CONFIG = {
     github: {
       display: true, // Display GitHub projects?
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'manual', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'updated', // Sort projects by 'stars' or 'updated'
         limit: 8, // How many projects to display.
@@ -25,7 +25,8 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: [''], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['dillon-luong/genregraph', 'dillon-luong/Foodie-Backend', 'dillon-luong/gsr.github.io-site', 
+                   'Jdmg1308/Hit-Me', 'nilava1234/VR-Arena-Shooter'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
