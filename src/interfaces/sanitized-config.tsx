07aa0d0fcf616d4d6be9ad1a2b303b1c17db7374
Dillon Luong <60891309+dillon-luong@users.noreply.github.com>
2026-09-13 19:28:26 -0400

    Add youtubeUrl property to SanitizedConfig interface

--- src/interfaces/sanitized-config.tsx
+++ src/interfaces/sanitized-config.tsx
@@ -24,6 +24,7 @@ export interface SanitizedExternalProject {
   description?: string;
   imageUrl?: string;
   link: string;
+  youtubeUrl?: string;
 }
 
 export interface SanitizedExternalProjects {
