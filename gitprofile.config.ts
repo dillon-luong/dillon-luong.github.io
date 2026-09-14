b35ad4e6c34d9a3ae94bfd53af7a2da7884071c4
Dillon Luong <60891309+dillon-luong@users.noreply.github.com>
2026-09-13 20:50:12 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -38,7 +38,7 @@ const CONFIG = {
           description:
             '3D Roguelike centered on Golf mechanics, built on themes of Golf, Retro Television, and Dark Fantasy (11 person team)',
           imageUrl:
-            'src/assets/gsr-game-title.png',
+            './src/assets/gsr-game-title.png',
           link: 'https://pseudomancer.studio/',
         },
         {
@@ -53,7 +53,7 @@ const CONFIG = {
           description:
             '2D action platformer emphasizing arcadey physics-based combat and gambling gameplay loop (6 person team)',
           youtubeURL:
-            'https://www.youtube.com/watch?v=CAAcnSCjZSY&t=3s',
+            'https://youtu.be/CAAcnSCjZSY',
           link: 'https://jdmg1308.itch.io/hit-me',
         },
         {
@@ -69,7 +69,7 @@ const CONFIG = {
           description:
             'WIP Music discovery platform that generates interactive visualizations of user listening habits using genre similarity analysis',
           imageUrl:
-            'src/assets/genregraph-ss.png',
+            './src/assets/genregraph-ss.png',
           link: 'https://genregraph.vercel.app/',
         },
       ],
